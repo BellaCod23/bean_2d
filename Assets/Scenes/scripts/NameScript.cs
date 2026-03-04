aa2849578c220a02b11363d29d220b329ca4148e
BellaCod23 <bellatattoo@inbox.lv>
2026-03-04 21:57:36 +0200

    Pirmās dienas beigas

--- Assets/Scenes/scripts/NameScript.cs
+++ Assets/Scenes/scripts/NameScript.cs
@@ -14,6 +14,8 @@ public class NameScript : MonoBehaviour
     public GameObject outputField;
     public GameObject reverseTextToggle;
 
+// Pirmās dienas beigas
+
     public void GetText()
     {
         randomIx = Random.Range(0, sampeText.Length);
